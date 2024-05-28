1a8960f605e3a030e203567fed556fd8bed31dca
Mats Gustafsson <113367401+Moonchichiii@users.noreply.github.com>
2024-05-28 19:59:21 +0200

    Redeployment to heroku.

--- backend/settings.py
+++ backend/settings.py
@@ -118,14 +118,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'backend.wsgi.application'
 
 
-#DATABASES = {'default': dj_database_url.config(default=config('DATABASE_URL'))}
+DATABASES = {'default': dj_database_url.config(default=config('DATABASE_URL'))}
 
-DATABASES = {
-         'default': {
-             'ENGINE': 'django.db.backends.sqlite3',
-             'NAME': BASE_DIR / 'db.sqlite3',
-         }
-     }
+# DATABASES = {
+#          'default': {
+#              'ENGINE': 'django.db.backends.sqlite3',
+#              'NAME': BASE_DIR / 'db.sqlite3',
+#          }
+#      }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
